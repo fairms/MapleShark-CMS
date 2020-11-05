71d678569a1bce0b5bbc3c035b62c5bfd6c3dbc5
fair <wanrenjie1@gmail.com>
2020-11-06 00:11:46 +0800

    添加项目文件。

---
+++ Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapleShark
+{
+    public static class Constants
+    {
+        public static ushort OpcodeTableOpcode => 0x002E;
+
+        public static string OpcodeTableKey => "M@PleStoryMaPLe!";
+    }
+}
